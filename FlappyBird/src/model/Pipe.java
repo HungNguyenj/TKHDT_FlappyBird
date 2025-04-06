a7efceecb150e9c316afea980dbee3cd66b81f23
HungNg <21130367@st.hcmuaf.edu.vn>
2025-04-06 21:47:14 +0700

    generate model for flappy bird

---
+++ FlappyBird/src/model/Pipe.java
@@ -0,0 +1,71 @@
+package model;
+
+import java.awt.*;
+
+public class Pipe {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private Image image;
+    private boolean passed = false;
+
+    public Pipe(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public Pipe(Image image) {
+        this.image = image;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public boolean isPassed() {
+        return passed;
+    }
+
+    public void setPassed(boolean passed) {
+        this.passed = passed;
+    }
+}
---
+++ FlappyBird/src/view/GameView.java
@@ -0,0 +1,8 @@
+package view;
+
+public class GameView {
+    private final int boardWidth = 360;
+    private final int boardHeight = 640;
+
+
+}
